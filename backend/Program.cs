c739629483da61f595990a99f8be09b01c93567e
danVadet <danielvieiramelolima@gmail.com>
2025-04-06 12:31:02 -0300

    Reajuste do local da pasta Images

--- backend/Program.cs
+++ backend/Program.cs
@@ -36,11 +36,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+/*
 app.UseStaticFiles(new StaticFileOptions {
     FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "Images")),
     RequestPath = "/Images"
 });
-
+*/
 app.UseRouting();
 
 app.UseAuthorization();
