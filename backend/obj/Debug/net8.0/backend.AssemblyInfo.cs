bc212ec78dec94d1511ebd64dab2b58f9fd5d3ae
Daniel  Vieira <danielvieiramelolima@gmail.com>
2025-06-13 14:22:39 -0300

    mi4u-mobile

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56425987a467c2169e58b544870cff9468c48f25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f04c6aa6476f7f3f4390059edfbf1afba177d204")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
