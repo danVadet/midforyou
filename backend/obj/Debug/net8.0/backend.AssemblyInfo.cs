0d50e9712136e05868af1e5202f19539e5d27174
Daniel  Vieira <danielvieiramelolima@gmail.com>
2025-07-18 09:18:43 -0300

    azure static

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49926b7555635b42a2a35fb46cc7d5db18dbc9a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b89e52894b2a042ce263c902324958c3433135d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
